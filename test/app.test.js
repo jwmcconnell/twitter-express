18dfb6d8897c4304d579b3f3f4263331448d6840
Jackmc <jackmc1122@gmail.com>
2019-07-11 09:44:35 -0700

    Return all tweets for get /tweets route.

--- test/app.test.js
+++ test/app.test.js
@@ -1,12 +1,26 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
+const expectedTweets = require('../lib/tweets.json');
+
 describe('Twitter clone static page', () => {
   it('returns a home page', () => {
     return request(app)
       .get('/')
       .then(res => {
         expect(res.text).toEqual(expect.stringContaining('<h1>Twitter</h1>'));
+      });
+  });
+});
+
+describe('Twitter routes', () => {
+  it('returns all tweets', () => {
+    return request(app)
+      .get('/tweets')
+      .then(res => {
+        const tweets = JSON.parse(res.text);
+        expect(tweets).toEqual(expectedTweets);
+        expect(tweets).toEqual(expect.any(Array));
       });
   });
 });
