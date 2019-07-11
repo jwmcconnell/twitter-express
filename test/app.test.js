694951361d85f4bb18e3133211efe3429adb906e
Jackmc <jackmc1122@gmail.com>
2019-07-11 10:52:27 -0700

    Add errors for having no handle or having no text in a posted tweet.

--- test/app.test.js
+++ test/app.test.js
@@ -9,6 +9,7 @@ describe('Twitter clone static page', () => {
       .get('/')
       .then(res => {
         expect(res.text).toEqual(expect.stringContaining('<h1>Twitter</h1>'));
+        expect(res.status).toEqual(200);
       });
   });
 });
@@ -21,6 +22,7 @@ describe('Twitter routes', () => {
         const tweets = JSON.parse(res.text);
         expect(tweets).toEqual(expectedTweets);
         expect(tweets).toEqual(expect.any(Array));
+        expect(res.status).toEqual(200);
       });
   });
 
@@ -31,6 +33,26 @@ describe('Twitter routes', () => {
       .then(res => {
         expect(res.body).toEqual({ handle: 'bob', text: 'I am test tweet', _id: 1 });
         expect(res.body).toEqual(expect.any(Object));
+      });
+  });
+
+  it('returns an error for a tweet without a handle', () => {
+    return request(app)
+      .post('/tweets')
+      .send({ tweet: { handle: '', text: 'I am test tweet' } })
+      .then(res => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual('Please provide a handle with your tweet.');
+      });
+  });
+
+  it('returns an error for a tweet without text', () => {
+    return request(app)
+      .post('/tweets')
+      .send({ tweet: { handle: 'jack', text: '' } })
+      .then(res => {
+        expect(res.status).toEqual(400);
+        expect(res.body).toEqual('Please provide some text for your tweet.');
       });
   });
 });
