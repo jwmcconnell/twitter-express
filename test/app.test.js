6f1fd27884fb76d1bfc09a8d8b90d677f074be0e
Jackmc <jackmc1122@gmail.com>
2019-07-11 10:37:22 -0700

    create post tweets route and test.

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-const expectedTweets = require('../lib/tweets.json');
+const expectedTweets = require('../lib/tweets.json').tweets;
 
 describe('Twitter clone static page', () => {
   it('returns a home page', () => {
@@ -21,6 +21,16 @@ describe('Twitter routes', () => {
         const tweets = JSON.parse(res.text);
         expect(tweets).toEqual(expectedTweets);
         expect(tweets).toEqual(expect.any(Array));
+      });
+  });
+
+  it('creates a tweet', () => {
+    return request(app)
+      .post('/tweets')
+      .send({ tweet: { handle: 'bob', text: 'I am test tweet' } })
+      .then(res => {
+        expect(res.body).toEqual({ handle: 'bob', text: 'I am test tweet', _id: 1 });
+        expect(res.body).toEqual(expect.any(Object));
       });
   });
 });
