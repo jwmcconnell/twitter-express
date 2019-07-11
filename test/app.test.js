9e8301123969d4667ebcc7b755c29d1c8082cfd4
Jackmc <jackmc1122@gmail.com>
2019-07-11 09:19:55 -0700

    Make basic server.

---
+++ test/app.test.js
@@ -0,0 +1,12 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('Twitter clone static page', () => {
+  it('returns a home page', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual(expect.stringContaining('<h1>Twitter</h1>'));
+      });
+  });
+});
